0c18575c90ebe36874905611ffc248d2e4c4990a
Madhav-stack-art <maddykk914@gmail.com>
2025-01-17 23:19:51 +0530

    Add files via upload

---
+++ createxyz-project/src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "d4221335-3210-4228-88ba-e499e06489b5");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
